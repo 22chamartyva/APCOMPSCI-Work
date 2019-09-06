8de088ffcc7496c96d012c53f8e467ffbe9cb8c9
Varun Chamarty <22chamartyva@fpsct.org>
2019-09-06 10:43:40 -0400

    REView while

---
+++ APCS/ReviewWhileLoops.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReviewWhileLoops
+{
+  public static void main()
+  {
+    Scanner thisScanner= new Scanner(System.in);
+    int Menu = thisScanner.nextInt();
+    while(Menu == 1){
+    System.out.println("HERRO CHILD");
+    thisScanner.nextInt();
+    }
+    while(Menu ==2){
+    System.out.println("Ma favorite COlllor is REED");
+    thisScanner.nextInt();
+    }
+    
+    
+    
+    
+  }
+}
--- APCS/truthTable.java
+++ APCS/truthTable.java
@@ -7,8 +7,8 @@ public static void main()
     Scanner thisScanner= new Scanner(System.in);
     
     
-    System.out.print("Do you have any pets besides a bird,dog, or cat?");      
-     
+    System.out.print("Do you have any pets?");      
+   
     
     
     
