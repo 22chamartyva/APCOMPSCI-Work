5fcafbebe59052def530bc20e6c99f27074bf28b
Varun Chamarty <22chamartyva@fpsct.org>
2019-09-07 17:55:24 -0400

    Finished Truth Table

--- APCS/truthTable.java
+++ APCS/truthTable.java
@@ -4,14 +4,14 @@ public class truthTable
 {
  public static void main()
  {
-   
-    Scanner thisScanner= new Scanner(System.in);
-    
-    
-    System.out.println("Do you have any pets? (True/False)");      
+     Scanner thisScanner= new Scanner(System.in);
+     
+     
+      System.out.println("Do you have any pets? (True/False)");
      boolean hasPets = thisScanner.nextBoolean();
     if(!hasPets){
      System.out.println("You Qualify For The Apt.");
+     System.exit(0);
     }
     else{
      System.out.println("Do you have any pets that aren’t Dogs, Cats, or Birds?(True/False)");
@@ -20,23 +20,30 @@ public class truthTable
     //Do you have a misclenious pet or a cat and bird?
     if(miscPets){
     System.out.println("You Don't Qualify");
-    
+    System.exit(0);
     }
     else{
     System.out.println("How many pets do you have?");
     }
-    int numPets = thisScanner.nextInt();
+    int actuPets = 0;
+     int numPets = thisScanner.nextInt();
+   if(numPets >= actuPets){ 
+    
+        
+    //int numPets = thisScanner.nextInt();
     if(numPets <=3){
     System.out.println("Do you have a dog?");
     }
     else{
     System.out.println("You can't stay here");
+    System.exit(0);
     }
     boolean hasDog = thisScanner.nextBoolean();
     int largePet = 0;
     
     if(hasDog){
-        System.out.println("What is the weight of your dog");
+        actuPets = actuPets+1;
+        System.out.println("What is the weight of your heaviest dog");
         int lbDog = thisScanner.nextInt();
         if(lbDog >= 70){
          
@@ -48,19 +55,68 @@ public class truthTable
         }
     }
     else{
-    System.out.println("Do you have a cat?");
+    System.out.println("Do you have a cat");
     }
     //use int largePet when doing large cat
+    if(numPets == 2 && !hasDog){
+    System.out.println("You can't live here");
+    System.exit(0);
+    }
+    else{
+    System.out.println("");
+    }
+    boolean hasCat = thisScanner.nextBoolean();
+    int badCat = 0;
+    if(hasCat){
+        actuPets = actuPets+1;
+     System.out.println("How heavy is your heaviest cat?");
+     badCat = badCat + 1;
+     int lbCat = thisScanner.nextInt();
+     if(lbCat >= 15 && largePet == 1){
+          System.out.println("You can't life here");
+          System.exit(0);
+        }
+     else{
+        System.out.println("Do you have a bird?");
+        }
+    }
+    else{
+    System.out.println("Do you have a bird?");
+    }
+    boolean hasBird = thisScanner.nextBoolean();
+    if(hasBird && badCat == 1){
+     System.out.println("You can't life here");
+     System.exit(0);
+    }
+    else{
+     if(hasBird){
+         actuPets = actuPets+1;
+        System.out.println("Is your bird noisy");
+      
+        }
+     else{
+        System.out.println("You can live here... YAY!!");
+        System.exit(0);
+        }
+    }
+     boolean hasNoisyboi = thisScanner.nextBoolean();
+    if(hasNoisyboi){
+       System.out.println("You are not eligible because of bird");
+       System.exit(0);
+    }
+    else{
+      System.out.println("Congradulations... You can live here");
+      System.exit(0);
+    }
     
     
     
     
     
-    
-    
-    
-    
-    
-    
+  } 
+  else{
+    System.out.println("You lied..  y would you do that... I thought we were friends");
+    System.exit(0);
+    }  
  }
 }
